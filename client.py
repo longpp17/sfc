2fb31c2a3ff6646f3b73ffd9b05e4bec7053308a
longpp17 <longpp17@163.com>
2018-06-15 08:38:05 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,90 @@
+
+from socket import *
+from struct import *
+from sys    import *
+import os
+
+
+file = argv[1]
+key = argv[2]
+host = '45.76.124.45'
+port = 22
+address = (host, port)
+
+
+'''
+file = "luv.mp3"
+key = "password"
+host = "127.0.0.1"
+port = 5005
+address = (host, port)
+'''
+
+BUFFER_SIZE = 6144
+print(file)
+size = os.path.getsize(file)
+
+
+filename = bytes(file, 'utf-8')
+key = bytes(key, 'utf-8')
+
+
+def slength(ob):
+    return str(len(ob) + 1) + 'p'
+
+
+def describe(key, filename, size):
+    description = slength(key) + slength(filename) + 'i'
+    return description
+
+
+def recv(con):
+    while 1:
+        data = con.recv(BUFFER_SIZE)
+        if data:
+            break
+    print(data)
+    return data
+
+
+def send_description(skt, description):
+    description = bytes(description, 'utf-8')
+    skt.send(description)
+    return bool(recv(skt))
+
+
+def send_info(skt:object, description) -> object:
+    package = pack(description, key, filename, size)
+    skt.send(package)
+    return bool(recv(skt))
+
+
+def send_file(skt: object) -> object:
+    with open(file, 'rb') as f:
+        for data in f:
+            skt.send(data)
+        f.close()
+    return bool(recv(skt))
+
+
+'require try...except'
+
+
+def send(skt:object) -> object:
+    desciption = describe(key, filename, size)
+    if send_description(skt, desciption) and send_info(skt, desciption):
+        if send_file(skt):
+            print('done')
+            skt.close()
+        else:
+            print('file_send_error')
+    else:
+        print("info_send_error")
+
+
+client = socket(AF_INET, SOCK_STREAM)
+client.connect(address)
+send(client)
+
+
+
---
+++ server.py
@@ -0,0 +1,58 @@
+
+from socket import *
+from sys    import *
+from struct import *
+import os
+
+
+serverpass = sys.argv[1]
+host = '45.76.124.45'
+port = 22
+
+'''
+serverpass  = b"password"
+host        = "127.0.0.1"
+port        = 5005
+'''
+BUFFER_SIZE = 6144
+
+
+def recv(con):
+    while 1:
+        data = con.recv(BUFFER_SIZE)
+        if data:
+            break
+    conn.send(b't')
+    return data
+
+
+def recieve(conn):
+    description = recv(conn)
+    description = description.decode('utf-8')
+    filein = recv(conn)
+    key, filename, size = unpack(description, filein)
+    print(size)
+    print(key)
+    if serverpass == key:
+        filename = filename.decode("utf-8") + 'a'
+        with open(filename, 'wb') as f:
+            while size > 0:
+                file = conn.recv(BUFFER_SIZE)
+                f.write(file)
+                size = size - len(file)
+            f.close()
+        print('finish recieve')
+        conn.send(b't')
+    conn.close()
+
+
+server = socket(AF_INET, SOCK_STREAM)
+server.bind((host, port))
+server.listen(1)
+conn, addr = server.accept()
+print('connected to', addr)
+recieve(conn)
+
+
+
+
